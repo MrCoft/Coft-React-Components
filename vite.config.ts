c5b0c3494dfb9ea8ef8221846821eaf0d78b5d77
Ondřej Műller <devcoft@gmail.com>
2023-09-04 15:58:33 +0200

    build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
+import {resolve} from 'node:path';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as packageJson from './package.json';
+import tsconfigPaths from "vite-tsconfig-paths";
+import svgr from "vite-plugin-svgr";
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    svgr(),
+    react(),
+    tsconfigPaths(),
+    dts({
+      include: ['src/components/', 'src/index.d.ts'],
+    })
+  ],
+  build: {
+    lib: {
+      entry: resolve('src', 'index.ts'),
+      formats: ['es'],
+      fileName: (format) => `coft-react-components.${format}.js`,
+    },
+    rollupOptions: {
+      external: [...Object.keys(packageJson.peerDependencies)],
+    },
+    sourcemap: true,
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src'),
+      }
+    }
+  }
 })
